0be82f3f4e8ccf44cb5a0226fc86db0008bde18d
Fabiano Santos Conrado <conradoinscricoes@gmail.com>
2022-09-14 15:56:31 -0300

    Add Banner!

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tela_de_login/views/home_page.dart';
 import 'package:tela_de_login/views/login_page.dart';
 import 'package:tela_de_login/views/splash_page.dart';
 
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   runApp(
     MaterialApp(
       initialRoute: '/splash',
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import '../service/ad_help.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,9 +11,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late BannerAd _bottomBannerAd;
+  bool isbottomBannerAdLoad = false;
+
+  void _createBottomBannerAd() {
+    _bottomBannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: AdHelp.bannerAdUnitId,
+      request: AdRequest(),
+      listener: BannerAdListener(onAdLoaded: (_) {
+        setState(() {
+          isbottomBannerAdLoad = true;
+        });
+      }, onAdFailedToLoad: (ad, error) {
+        ad.dispose();
+      }),
+    );
+    _bottomBannerAd.load();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _createBottomBannerAd();
+
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _bottomBannerAd.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: isbottomBannerAdLoad ? Container(
+        height: _bottomBannerAd.size.height.toDouble(),
+        width: _bottomBannerAd.size.width.toDouble(),
+        child: AdWidget(ad: _bottomBannerAd),
+      ) : null,
       body: Center(
         child: Text("Home Page"),
       ),
